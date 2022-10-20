46eae9a925f65ecc9462430d453f6c340f7585ad
aneeshkatta <113498885+aneeshkatta@users.noreply.github.com>
2022-10-21 01:09:29 +0530

    Add files via upload
    added uc1

---
+++ UserCase01EmpWage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserCase01EmployeeWage
+{
+    internal class UserCase01EmpWage
+    {
+        
+        
+        public void EmpCheck()
+        {
+            int IS_Fulltime = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+
+
+            if (empCheck == IS_Fulltime)
+            {
+                Console.WriteLine(" Employee is present");
+             }
+            else
+            {
+                Console.WriteLine(" Employee is absent");
+
+            }
+          
+        }
+        public static void Main(string[] args)
+{
+            UserCase01EmpWage EmpWagMethod = new UserCase01EmpWage();
+            EmpWagMethod.EmpCheck();
+
+        }
+    }
+}
