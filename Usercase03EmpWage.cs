0d4a9ea257c2ae12627b112d167059975759adc2
aneeshkatta <113498885+aneeshkatta@users.noreply.github.com>
2022-10-21 01:12:02 +0530

    Add files via upload
    added uc3

---
+++ Usercase03EmpWage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Usercase03EmployeeWage
+{
+    class Usercase03EmpWage
+    {
+        public void EmpWage03()
+        {
+                int IS_Parttime = 0;
+                int IS_Fulltime = 1;
+                int EMP_RATE_PER_HOUR = 100;
+                int emphrs = 0;
+                int empwage = 0;
+                int totalempwage = 0;
+                Random random = new Random();
+                int empCheck = random.Next(0, 2);
+
+
+            if (empCheck == IS_Parttime)
+            {
+                emphrs = 4;
+            }
+            else if (empCheck == IS_Fulltime)
+            {
+                emphrs = 8;
+            }
+            else 
+            { 
+                    emphrs = 0;
+                    
+            }
+            empwage = emphrs * EMP_RATE_PER_HOUR;
+            totalempwage += empwage;
+
+            Console.WriteLine(" Employee Wage is " + empwage);
+
+             }
+        public static void  Main(string[] args)
+        {
+            Usercase03EmpWage Case03EmpWage = new Usercase03EmpWage();
+            Case03EmpWage.EmpWage03();
+        }
+
+    }
+}
